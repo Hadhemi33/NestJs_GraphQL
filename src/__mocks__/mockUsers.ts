4e41b30ca9edca360dcec639d9c171545142f60a
Hadhemi33 <benmansourhadhemi01@gmail.com>
2024-02-26 12:15:24 +0100

    Query Arguments

---
+++ src/__mocks__/mockUsers.ts
@@ -0,0 +1,17 @@
+export const mockUsers = [
+  {
+    id: 1,
+    username: 'anson',
+    displayName: 'Anson The Developer',
+  },
+  {
+    id: 2,
+    username: 'Jack',
+    displayName: 'Jack The Developer',
+  },
+  {
+    id: 3,
+    username: 'Anna',
+    displayName: 'Anna The Developer',
+  },
+];
