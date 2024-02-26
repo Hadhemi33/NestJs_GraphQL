25a84531590f800fe56a12b1ed4ca1459dc0ed46
Hadhemi33 <benmansourhadhemi01@gmail.com>
2024-02-26 15:20:14 +0100

    Resolving Field

--- src/graphql/resolvers/UserResolver.ts
+++ src/graphql/resolvers/UserResolver.ts
@@ -1,15 +1,30 @@
-import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Int,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
 import { User } from '../models/User';
 import { mockUsers } from 'src/__mocks__/mockUsers';
+import { UserSetting } from '../models/UserSettings';
+import { of } from 'rxjs';
+import { mockUserSettings } from 'src/__mocks__/mockUserSettings';
 
-@Resolver()
+@Resolver((of) => User)
 export class UserResolver {
   @Query((returns) => User, { nullable: true })
   getUserById(@Args('id', { type: () => Int }) id: number) {
     return mockUsers.find((user) => user.id === id);
   }
-  @Query((returns) => [User])
+  @Query(() => [User])
   getUsers() {
     return mockUsers;
+  }
+  @ResolveField((returns) => UserSetting, { name: 'settings', nullable: true })
+  getUserSettings(@Parent() user: User) {
+    console.log(user);
+    return mockUserSettings.find((setting) => setting.userId === user.id);
   }
 }
